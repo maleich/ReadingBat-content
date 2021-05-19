845be19fa0dd95538297dcdee29ad3181b1a159b
mleich <mleich@athenian.org>
2021-05-18 20:14:11 -0700

    edit student exercises

--- python/grab_bag/geometry1.py
+++ python/grab_bag/geometry1.py
@@ -1,14 +1,14 @@
 # @desc Add a short description or instruction here. This will show up at the top of the exercise.
 
-def pythagSolve(side_A, side_B):     # give your function a name and parameter(s)
+def pythagSolve(side_A, side_B):
     side_A = side_A ** 2
     side_B = side_B ** 2
-    side_C= (side_A+side_B) ** 0.5
-    return  side_C  # what does it return? This will be what the user types when they predict the result.
+    side_C= (side_A + side_B) ** 0.5
+    return  side_C
 
 
 def main():
-    print(pythagSolve(1, 1))
+    print(pythagSolve(8, 6))
     print(pythagSolve(3, 4))
     print(pythagSolve(4, 3))
     print(pythagSolve(5, 12))
